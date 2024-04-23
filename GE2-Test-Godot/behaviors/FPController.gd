f6d728e983d3a74fd268214228475d97d945f1fd
Michael Cooper <C20321456@mytudublin.ie>
2024-04-23 10:19:44 +0100

    added the fish body to the scene

--- GE2-Test-Godot/behaviors/FPController.gd
+++ GE2-Test-Godot/behaviors/FPController.gd
@@ -8,10 +8,27 @@ extends Node3D
 @export var sensitivity = 0.1
 @export var speed:float = 1.0
 
+var move_and_slide
+
 var controlling = true
 
 var left:XRController3D
 var right:XRController3D
+
+var length
+var frequncy
+var start_angle
+var base_size
+var multiplier
+var velocity 
+
+var target
+var slowing_radius
+
+var boid
+var feeler
+
+var pause
 
 
 func _input(event):
@@ -25,6 +42,9 @@ func _input(event):
 			Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 		controlling = ! controlling
+	
+	#if event.is_action_pressed("pause"):
+		
 
 
 # Called when the node enters the scene tree for the first time.
@@ -33,6 +53,24 @@ func _ready():
 	pass # Replace with function body.
 
 @export var can_move:bool = true
+
+func _redraw(gizmo):
+	gizmo.clear()
+
+	var node3d = gizmo.get_node_3d()
+
+	var lines = PackedVector3Array()
+
+	lines.push_back(Vector3(0, 1, 0))
+	lines.push_back(Vector3(0, node3d.my_custom_value, 0))
+
+	var handles = PackedVector3Array()
+
+	handles.push_back(Vector3(0, 1, 0))
+	handles.push_back(Vector3(0, node3d.my_custom_value, 0))
+
+	DebugDraw3D.draw_sphere(target.global_transform.origin, slowing_radius, Color.VIOLET)
+	DebugDraw3D.draw_line(boid.global_transform.origin, feeler.hit_target, Color.CHARTREUSE)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -61,3 +99,4 @@ func _process(delta):
 		var upanddown = Input.get_axis("move_up", "move_down")
 		if abs(upanddown) > 0:     
 			global_translate(- global_transform.basis.y * speed * upanddown * mult * delta)
+
